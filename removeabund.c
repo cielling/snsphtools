6264593c97cbb86311360d9e8f3ff926cd0181d4
Carola I. Ellinger <carola.ellinger@gmail.com>
2011-12-05 12:38:00 -0700

    create a routine to strip abundance information from an sdf
    The routine just strips the abundance information from the "end" of
    the SPHbody structure. This could be useful, e.g. to add different
    abundances, or to calculate u from T via addabundance2v3 (since that
    requires knowing the particle density, i.e. chemical makeup, of each
    particle, so it is best done when adding abundances).
    
    to do: strip abundance info from header

---
+++ removeabund.c
@@ -0,0 +1,298 @@
+/*
+   PURPOSE:
+	to remove abundance information from an sdf
+        This version should also work for large files, since it reads in the
+        sdf file line-by-line.
+
+   NOTE:
+    - assumes that the abundances are the last members in the SPHbody struct.
+    - does not remove abundance info from header yet, which can/ has to be done
+      manually
+
+   COMPILE:
+    with Makefile2. un-comment appropriate lines.
+    This routine needs some libraries from the tree code and SDF routines,
+    so make sure that TREEHOME is set, and the tree code (SNSPH) compiled
+    once for serial use (i.e. without PAROS flag).
+
+   RUN:
+    addvel <in-file.sdf> <out-file.sdf>
+
+   METHOD:
+	@ read in header of <in-file.sdf>
+    @ determine where the abundance information starts
+	@ read in data line-by-line, omitting the abundances
+	@ write data to <out-file.sdf>
+
+   TO DO:
+    - remove abundance info from header
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <math.h>
+#include <SDF.h>
+
+
+typedef enum SDF_type_enum SDF_type_t;
+
+typedef union {
+    int i;
+    float f;
+    double d;
+} datum_t;
+
+static void initargs(int argc, char *argv[], SDF **sdfp, FILE **fp);
+static void writeinit(FILE *fp);
+static void writescalars(SDF *sdfp, FILE *fp);
+static void writestructs(SDF *sdfp, FILE *fp);
+
+
+int main(int argc, char *argv[])
+{
+    SDF *sdfp = NULL;
+    FILE *fp = NULL;
+
+    initargs(argc, argv, &sdfp, &fp);
+
+    writeinit(fp);
+    writescalars(sdfp, fp);
+    writestructs(sdfp, fp);
+
+    fclose(fp);
+    SDFclose(sdfp);
+
+    return 0;
+}
+
+static void initargs(int argc, char *argv[], SDF **sdfp, FILE **fp)
+{
+    char input;
+
+    if (argc != 3) {
+	fprintf(stderr, "Usage: %s SDFfile outfile\n", argv[0]);
+	exit(1);
+    }
+
+    *sdfp = SDFopen(NULL, argv[1]);
+    if (*sdfp == NULL) {
+	fprintf(stderr, "%s: %s: %s\n", argv[0], argv[1], SDFerrstring);
+	exit(2);
+    }
+
+    if (access(argv[2], F_OK) == 0) {
+        fprintf(stderr, "%s: %s exists; overwrite (y/n)? ", argv[0],
+		argv[2]);
+        input = getc(stdin);
+        if ((input != 'y') && (input != 'Y'))
+            exit(3);
+    }
+    *fp = fopen(argv[2], "w");
+    if (*fp == NULL) {
+	fprintf(stderr, "%s: %s\n", argv[2], strerror(errno));
+	exit(errno);
+    }
+}
+
+static void writeinit(FILE *fp)
+{
+    fprintf(fp, "# SDF\n");
+    fprintf(fp, "parameter byteorder = %#x;\n", SDFcpubyteorder());
+}
+
+static void writescalars(SDF *sdfp, FILE *fp)
+{
+    int i, nvecs, nrecs;
+    int flag = 0;
+    char **vecs;
+    SDF_type_t type;
+    datum_t datum;
+
+    /*figure out number of lines in the header, basically-CIE*/
+    nvecs = SDFnvecs(sdfp);
+    /*get the names of the variables/parameters in the header-CIE*/
+    vecs = SDFvecnames(sdfp);
+
+    /*go through all of them individually-CIE*/
+    for (i = 0; i < nvecs; ++i) {
+        /*figure out if scalar(=1) or array(!=1)?-CIE*/
+        if (strncmp(vecs[i], "x", strlen(vecs[i])) == 0) flag = 1;
+	if (flag) continue;
+
+	type = SDFtype(vecs[i], sdfp);
+	/* Doesn't handle string */
+	/* Also, SDFgetfloat was happy to read a double scalar and
+	   convert it for me; that probably works backwards too.  I
+	   don't think there's an equivalent for the SDFrdvecs family
+	   though, so "read; convert type; write" is the general
+	   path. */
+    /*read in header file, line by line, with the appropriate function-CIE*/
+	switch (type) {
+	case SDF_INT:
+	    SDFgetint(sdfp, vecs[i], &(datum.i));
+	    break;
+	case SDF_FLOAT:
+	    SDFgetfloat(sdfp, vecs[i], &(datum.f));
+	    break;
+	case SDF_DOUBLE:
+	    SDFgetdouble(sdfp, vecs[i], &(datum.d));
+	    break;
+	default:
+	    fprintf(stderr, "%s: type not supported\n", vecs[i]);
+/* 	    exit(-1); */
+	}
+
+    /*write header file, line by line, as the appropriate data type-CIE*/
+	switch (type) {
+	case SDF_INT:
+	    fprintf(fp, "int %s = %d;\n", vecs[i], datum.i);
+	    break;
+	case SDF_FLOAT:
+	    fprintf(fp, "float %s = %.7g;\n", vecs[i], datum.f);
+	    break;
+	case SDF_DOUBLE:
+	    fprintf(fp, "double %s = %.16g;\n", vecs[i], datum.d);
+	    break;
+	default:
+	    fprintf(stderr, "%s: type not supported\n", vecs[i]);
+/* 	    exit(-1); */
+	}
+    }
+}
+
+static void writestructs(SDF *sdfp, FILE *fp)
+{
+    FILE *frp = NULL;
+    int i, j, nvecs, nmembers, nmembersA;
+    int counter, flag = 0, flag2 = 0;
+    int nrecs;
+    int *strides, *nobjs, *starts, *inoffsets;
+    char **vecs, **members;
+    SDF_type_t *types;
+    size_t stride = 0, instride = 0;
+    void *btab;
+    void **addrs;
+
+    frp = fopen("output/log.out", "w");
+    if(!frp) printf("error opening log file!\n");
+
+
+    SDFgetint(sdfp, "npart", &nrecs);
+    fprintf(frp,"%d particles\n", nrecs);
+
+    nvecs = SDFnvecs(sdfp);
+    vecs = SDFvecnames(sdfp);
+
+    /* Count structure members */
+    for (i = 0, nmembers = 0, nmembersA = 0; i < nvecs; ++i) {
+        if (strncmp(vecs[i], "x", strlen(vecs[i])) == 0) flag = 1;
+	    if (flag && !flag2) ++nmembers;
+        /* stop counting nmembers once abundances are reached, but
+           count number of abundances */
+        if (strncmp(vecs[i], "f1", strlen(vecs[i])) == 0) flag2 = 1;
+        if(flag2) ++nmembersA;
+    }
+    fprintf(frp,"nmembers = %d nmembersA = %d\n",nmembers, nmembersA);
+
+
+    /*malloc memory space for the respective features of the struct-CIE*/
+    members = (char **)malloc(nmembers * sizeof(char *));
+    addrs = (void **)malloc(nmembers * sizeof(void *));
+    strides = (int *)malloc(nmembers * sizeof(int));
+    nobjs = (int *)malloc(nmembers * sizeof(int));
+    starts = (int *)malloc(nmembers * sizeof(int));
+    types = (SDF_type_t *)malloc(nmembers * sizeof(SDF_type_t));
+    inoffsets = (int *)malloc(nmembers * sizeof(int));
+
+    /*one by one, go through all the fields in the column, i.e. members of the struct?-CIE*/
+    flag = 0;
+    flag2 = 0;
+    for (i = 0, nmembers = 0, stride = 0; i < nvecs; ++i) {
+        if (strncmp(vecs[i], "x", strlen(vecs[i])) == 0) flag = 1;
+        if (strncmp(vecs[i], "f1", strlen(vecs[i])) == 0) flag2 = 1;
+        if (flag && !flag2) {
+	        members[nmembers] = vecs[i];
+	        nobjs[nmembers] = 1;/*nobjs[0] is the number of particles. are all elements
+                                      in this nobjs array the same then??-CIE:yes, but can be
+                                      different*/
+            starts[nmembers] = 0;  /* Not correct in parallel; use NobjInitial */
+            types[nmembers] = SDFtype(members[nmembers], sdfp);
+	        inoffsets[nmembers] = stride;
+	        stride += SDFtype_sizes[types[nmembers]];
+	        instride += SDFtype_sizes[types[nmembers]];
+
+	        ++nmembers;
+	    }
+        /* SDFseekreadvecsarr needs the stride between successive members in the
+           input file, so keep counting */
+        if (flag2 && flag) {
+            instride += SDFtype_sizes[ SDFtype(vecs[i], sdfp) ];
+        }
+    }
+
+    btab = (void *)malloc(stride);
+
+    /*calculate the byte offset in memory to the address of the next member-CIE*/
+    for (i = 0; i < nmembers; ++i) {
+	    addrs[i] = (char *)btab + inoffsets[i];
+	    strides[i] = instride;
+    }
+
+
+    /*print the struct declaration part from the header-CIE*/
+    fprintf(fp, "struct {\n");
+    for (i = 0; i < nmembers; ++i) {
+	switch (types[i]) {
+	case SDF_INT:
+	    fprintf(fp, "\tint %s;\n", members[i]);
+	    break;
+	case SDF_FLOAT:
+            fprintf(fp, "\tfloat %s;\n", members[i]);
+	    break;
+	case SDF_DOUBLE:
+	    fprintf(fp, "\tdouble %s;\n", members[i]);
+	    break;
+	default:
+	    fprintf(stderr, "%s: type not supported\n", members[i]);
+	    exit(-1);
+	}
+    }
+    fprintf(fp, "}[%d];\n", nrecs);
+    fprintf(fp, "#\n");
+    fprintf(fp, "# SDF-EOH\n");
+
+    /* read in the data, omitting abundances */
+    for (j = 0; j < nrecs; ++j) {
+
+        for( i = 0; i < nmembers; i++) starts[i] = j;
+
+        /* read one line of data into btab (via addrs) */
+        SDFseekrdvecsarr(sdfp, nmembers, members, starts, nobjs, addrs,
+		     strides);
+
+        /*dump the outbtab data into the file now-CIE*/
+        fwrite(btab, stride, 1, fp);
+
+        /* write counter to screen, so we know where the read is */
+        if(!(j%100000)) printf("processed line %d\n", j);
+
+    }
+    fprintf(frp,"wrote %d abundances to file\n", counter);
+
+    /*and we're done! clean up now -CIE*/
+    free(members);
+    free(addrs);
+    free(strides);
+    free(nobjs);
+    free(starts);
+    free(types);
+    free(inoffsets);
+
+    free(btab);
+}
+
+
