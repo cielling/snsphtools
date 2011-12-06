aa8c9c01fe8d7e10fef28a36132317f683e88273
Carola I. Ellinger <carola.ellinger@gmail.com>
2011-12-06 14:02:37 -0600

    update header in SDFtoASCII-batch, plus miscellaneous edits and formatting
    as with SDFtoASCII, some parameters change from time to time, which are then
    recorded every now and then in git.

--- SDFtoASCII-batch.c
+++ SDFtoASCII-batch.c
@@ -1,12 +1,22 @@
 /*
    PURPOSE:
-	do the below for a whole bunch of files.
-	to convert SDF files to formatted text files that OpenDX can read.
-        only a few columns are written to the text file, namely x,y,z,h,mass,rho.
-	One line of the SDF file is read at a time, so this should work for any
-	size SDF file.
-   NOTE: this code assumes that 'npart' from the header contains the total number
-	of particles.
+    this is the "batch" version of SDFtoASCII. You could probably write a 
+    script that calls SDFtoASCII a bunch of times. Or you can do it this way....
+
+   NOTE:
+    - this code assumes that 'npart' from the header contains the total number
+    of particles.
+    - this routine can handle 4- or 5-digit file extensions to the sdf files, 
+    but not both at the same time (yet?), so if you have more that 9999 time 
+    steps, you have to call it twice, once for <9999, once for >9999.
+    - does not have a switch to calculate log values of quantities
+    - the program will prompt you for the first time step, the last time step
+    (inclusive), and the time step increment to read in; e.g. entering '0 1000 10'
+    at the prompt will read in the sdf dumps '..._sph.0000', '..._sph.0010', ...,
+    '..._sph.1000'. 
+
+   SYNTAX:
+    SDFtoASCII-batch <sdfname-base> <outname-base>
 
    DONE: 1) get list of names in SDF file
    DONE: 2) read all scalars
@@ -14,6 +24,11 @@
    DONE: 4) calculate offsets, write adjusted members into buffer
    DONE: 5) loop over 3-4 until whole file is read (or seg fault is reached ;-P)
    DONE: 6) write scalars and buffer to file
+
+   TO DO:
+    - prompt user for quantities of interest to get (leave x, y, z, in)
+    - make this independent of/scalable to arbitrary number of quantities
+    - add log functionality
 */
 
 #include <stdio.h>
@@ -73,12 +88,6 @@ int main(int argc, char *argv[])
 
     printf("enter numbers of: first-file last-file file-increment \n");
     scanf("%d %d %d", &start, &nfile, &INCR);
-    /*scanf("%d %d", &INCR, &nfile);*/
-/*
-    INCR = 10;
-    nfile = 2000;
-    start = 0;
-*/
     printf("%d %d\n", INCR, nfile);
 
     for( i = start; i <= nfile; i = i+INCR ) {
@@ -160,13 +169,13 @@ static void writestructs(SDF *sdfp, FILE *fp)
     int nlines = 1, nrecs;
     int index[num];
     double rx, ry, rz;
-    /*make INCR and nlines user input */
 
-/* this does not attempt to load the whole file into memory, does it? -CE */
+    /* this does not attempt to load the whole file into memory, does it? -CE */
     nvecs = SDFnvecs(sdfp);
     vecs = SDFvecnames(sdfp);
 
     SDFgetint(sdfp, "npart", &nrecs);
+    nrecs=8192;
 
     /* Count structure members */
     /* don't use SDFnrecs, since that reads in the entire file which I'm trying to
@@ -181,15 +190,15 @@ static void writestructs(SDF *sdfp, FILE *fp)
         }
         if (strncmp(vecs[i], "y", strlen(vecs[i])) == 0) index[1]=i;
         if (strncmp(vecs[i], "z", strlen(vecs[i])) == 0) index[2]=i;
-        if (strncmp(vecs[i], "f2", strlen(vecs[i])) == 0) index[3]=i;
-        if (strncmp(vecs[i], "f12", strlen(vecs[i])) == 0) index[4]=i;
-        if (strncmp(vecs[i], "f18", strlen(vecs[i])) == 0) index[5]=i;
+        if (strncmp(vecs[i], "vx", strlen(vecs[i])) == 0) index[3]=i;
+        if (strncmp(vecs[i], "vy", strlen(vecs[i])) == 0) index[4]=i;
+        if (strncmp(vecs[i], "vz", strlen(vecs[i])) == 0) index[5]=i;
         if (strncmp(vecs[i], "rho", strlen(vecs[i])) == 0) index[6]=i;
 	if (flag) ++nmembers;
     }
     printf("nmembers = %d\n",nmembers);
 
-/*malloc memory space for the respective features of the struct-CE*/
+    /*malloc memory space for the respective features of the struct-CE*/
     members = (char **)malloc(num * sizeof(char *));
     addrs = (void **)malloc(num * sizeof(void *));
     types = (SDF_type_t *)malloc(num * sizeof(SDF_type_t));
@@ -198,7 +207,7 @@ static void writestructs(SDF *sdfp, FILE *fp)
     lines = (int * )malloc(num * sizeof(int));
     starts = (int * )malloc(num * sizeof(int));
 
-/*one by one, go through the fields in the column, i.e. members of the struct?-CE*/
+    /*one by one, go through the fields in the column, i.e. members of the struct?-CIE*/
     for (i = 0, stride = 0; i < num; ++i) {
 	    members[i] = vecs[index[i]];
 	    types[i] = SDFtype(members[i], sdfp);
@@ -208,21 +217,23 @@ static void writestructs(SDF *sdfp, FILE *fp)
             lines[i] = nlines;
 	}
 
-    /* unnecesary, just use 'stride' ? CE */
+    /* unnecesary, just use 'stride' ? CIE */
     outstride = 0;
     for( i = 0; i < num; i++) outstride += SDFtype_sizes[ types[i] ];
 
     btab = (void *)malloc(stride * nlines);
 
-    /*calculate the byte offset in memory to the address of the next member-CE*/
+    /*calculate the byte offset in memory to the address of the next member-CIE*/
 	addrs[0] = (char *)btab;
 	for ( i = 1; i < num; i++) {
             addrs[i] = addrs[i-1] + SDFtype_sizes[ types[i-1] ];
             strides[i] = outstride;
         }
 
+/*
     for ( k = 0; k < num; k++) fprintf(fp,"% 13s", members[k]);
     fprintf(fp, "\n");
+*/
 
     printf("reading in %d lines ...\n",nrecs);
 
