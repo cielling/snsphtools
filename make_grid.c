3d4ad0125fa64bd19782d57a84b007055080aa26
ellinger <carola.ellinger@gmail.com>
2013-03-08 14:48:41 -0600

    adding logarithmic gridding option

--- make_grid.c
+++ make_grid.c
@@ -8,7 +8,7 @@
 
 //const int NDIM=3;
 
-void initargs(int argc, char* argv[], FILE** fp, int *nx, int *ny, int *nz);
+void initargs(int argc, char* argv[], FILE** fp, int *nx, int *ny, int *nz, int *gridopt);
 
 int main(int argc, char* argv[])
 {
@@ -16,29 +16,44 @@ int main(int argc, char* argv[])
 	SPHoutbody *outbtab, OneBody;
 	int i, j, k, index;
 	int nx, ny, nz;
+	int gridopt;
 	float nx2, ny2, nz2;
 	int ngrid;
 	float dx, dy, dz, dh;
 	float ii, jj, kk;
+	float rad;
 	float R0=4.e5;
 
 	/*initargs: check if file exists, ask if overwrite*/
-	initargs(argc, argv, &fp, &nx, &ny, &nz);
+	initargs(argc, argv, &fp, &nx, &ny, &nz, &gridopt);
 
 	printf("set nx= %d ny= %d nz= %d\n",nx, ny, nz);
+
+	if( gridopt == 0 ) printf("doing linear gridding\n");
+	else if (gridopt == 1) printf("doing logarithmic gridding\n");
+	else printf("grid option not supported\n");
 
 	/*create the grid */
 	ngrid = nx*ny*nz;
 	outbtab = malloc( ngrid*sizeof(SPHoutbody) );
 	if(!outbtab) fprintf(stderr, "error allocating outbtab!\n");
 	
-	dx = 2.*R0/(float)(nx-1);
-	dy = 2.*R0/(float)(ny-1);
-	dz = 2.*R0/(float)(nz-1);
+	if( gridopt == 0 ) {
+		dx = 2.*R0/(float)(nx-1);
+		dy = 2.*R0/(float)(ny-1);
+		dz = 2.*R0/(float)(nz-1);
+	} else if (gridopt == 1 ) {
+		dx = log10(R0)/(float)(nx-1)*2.;
+		dy = log10(R0)/(float)(ny-1)*2.;
+		dz = log10(R0)/(float)(nz-1)*2.;
+
+		//dh = pow( 10., (dx+dy+dz)/3.*0.5 );
+	}
+
 	nx2 = (float)(nx-1)*0.5;
 	ny2 = (float)(ny-1)*0.5;
 	nz2 = (float)(nz-1)*0.5;
-
+		
 	dh = (dx+dy+dz)/3. *0.5;
 
 	/* try different binning also, like log, sqrt */
@@ -54,19 +69,43 @@ int main(int argc, char* argv[])
 				kk = (float)k-nz2;
 				index = i*ny*nz + j*nz + k;
 				
-				outbtab[index].pos[0] = (double)ii*dx;
-				outbtab[index].pos[1] = (double)jj*dy;
-				outbtab[index].pos[2] = (double)kk*dz;
+				if( gridopt == 0 ) {
+					outbtab[index].pos[0] = (double)ii*dx;
+					outbtab[index].pos[1] = (double)jj*dy;
+					outbtab[index].pos[2] = (double)kk*dz;
+					outbtab[index].h = dh;
+				} else if ( gridopt == 1 ) {
+					if( ii<0 ) 
+					outbtab[index].pos[0] = (double)(-1.)*pow(10., fabs(ii*dx));
+					else
+					outbtab[index].pos[0] = (double)pow(10., ii*dx);
+
+					if( jj<0 ) 
+					outbtab[index].pos[1] = -1.*pow(10., fabs((double)jj*dx));
+					else
+					outbtab[index].pos[1] = pow(10., (double)jj*dx);
+
+					if( kk<0 ) 
+					outbtab[index].pos[2] = -1.*pow(10., fabs((double)kk*dx));
+					else
+					outbtab[index].pos[2] = pow(10., (double)kk*dx);
+
+					rad = sqrt( outbtab[index].pos[0]* outbtab[index].pos[0]+ 
+						outbtab[index].pos[1]* outbtab[index].pos[1]+ 
+						outbtab[index].pos[2]* outbtab[index].pos[2]); 
+					outbtab[index].h = rad*( pow(10., dh) -1.);
+				}
 				outbtab[index].mass = 1.;
-				outbtab[index].h = dh;
 				outbtab[index].rho = 0.;
-				/*
 				outbtab[index].nterms = 0.;
 				outbtab[index].temp = 0.;
 				outbtab[index].vel[0] = 0.;
 				outbtab[index].vel[1] = 0.;
 				outbtab[index].vel[2] = 0.;
+				outbtab[index].f2 = 0.;
+				outbtab[index].f3 = 0.;
 
+				/*
 				OneBody.pos[0] = (double)ii*dx;
 				OneBody.pos[1] = (double)jj*dy;
 				OneBody.pos[2] = (double)kk*dz;
@@ -123,12 +162,12 @@ int main(int argc, char* argv[])
 }
 
 
-void initargs(int argc, char* argv[], FILE** fp, int *nx, int *ny, int *nz) {
+void initargs(int argc, char* argv[], FILE** fp, int *nx, int *ny, int *nz, int *gridopt) {
 
 	char input;
 
-	if( argc != 5 ) {
-		fprintf(stderr, "Usage: %s outfile nx ny nz\n", argv[0]); 
+	if( argc != 6 ) {
+		fprintf(stderr, "Usage: %s outfile nx ny nz gridoption\n", argv[0]); 
 		exit(1);
 	}
 
@@ -155,5 +194,6 @@ void initargs(int argc, char* argv[], FILE** fp, int *nx, int *ny, int *nz) {
 	*nx = atoi(argv[2]);
 	*ny = atoi(argv[3]);
 	*nz = atoi(argv[4]);
+	*gridopt = atoi(argv[5]);
 
 }
